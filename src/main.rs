b841ee19493e9fd4d60e6ff25d0057a553d0bf8a
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-06-12 19:58:04 +1200

    Customizable jar file

--- src/main.rs
+++ src/main.rs
@@ -9,12 +9,25 @@ pub use events::*;
 use std::{
     io,
     path::Path,
-    thread
+    thread,
+    collections::HashMap
 };
+use config::Config;
 
-static JAR_PATH: &str = "minecraft_server.jar";
 fn main() {
     println!("Please wait...");
+
+    let config = Config::builder()
+        .add_source(config::File::with_name("config"))
+        .build()
+        .unwrap();
+    let config_data = config
+        .try_deserialize::<HashMap<String, String>>()
+        .unwrap();
+
+    let jar_file = config_data.get("jar_file").expect("error getting jar_file from config");
+
+    println!("Using jar file: {}", jar_file);
 
     thread::spawn(|| {
         loop {
@@ -27,12 +40,12 @@ fn main() {
         }
     });
 
-    if Path::new(JAR_PATH).exists() {
-        wrapper::init(JAR_PATH);
+    if Path::new(jar_file).exists() {
+        wrapper::init(jar_file);
     } else {
         println!("Jar file could not be found!");
         println!("Ensure that the Minecraft jar has the name:");
-        println!("{JAR_PATH}");
+        println!("{jar_file}");
         std::process::exit(1);
     }
 }
