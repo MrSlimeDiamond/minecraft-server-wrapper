0e4580c6291ffc8caca7cbc0a70d83f2994c46fd
alexzinyew <71101894+alexzinyew@users.noreply.github.com>
2023-06-14 23:51:53 -0400

    API for sending commands to the server (#3)
    * Module organization
    
    * Config error handling
    
    * Small text change
    
    * better wording
    
    * stuffs
    
    * API

--- src/main.rs
+++ src/main.rs
@@ -1,18 +1,25 @@
+#![feature(proc_macro_hygiene, decl_macro)]
+
 #[macro_use]
+extern crate rocket;
 mod wrapper;
 mod regex_checker;
 mod events;
+mod api;
 
 use std::{
     io::{self, Write},
     path::Path,
-    thread,
-    collections::HashMap, fs::File
+    collections::HashMap,
+    fs::File,
+    thread
 };
 use config::Config;
 use wrapper::send_cmd;
+use api::network;
 
-fn main() {
+#[tokio::main]
+async fn main() {
     println!("Please wait...");
 
     let config = Config::builder()
@@ -23,26 +30,28 @@ fn main() {
         Err(error) => match error.to_string().ends_with("not found") {
             true => {
                 println!(r#"Creating "wrapper_config.ini" with default values, you may want to change these!"#);
-                File::create("wrapper_config.ini").unwrap().write_all(b"jar_file = minecraft_server.jar\ngui = true\nxmx = -Xmx2G\nxms = -Xms2G").unwrap();
+                File::create("wrapper_config.ini").unwrap().write_all(
+                    b"jar_file = minecraft_server.jar\ngui = true\nxmx = -Xmx2G\nxms = -Xms2G\napi-port = 7867\napi-key = supersecretkey").unwrap();
                 Config::builder().add_source(config::File::with_name("wrapper_config")).build().unwrap()
             },
             false => panic!("Something went wrong with the config file")
         }
     };
 
-
     let config_data = config
         .try_deserialize::<HashMap<String, String>>()
         .unwrap();
 
-    let jar_file = config_data.get("jar_file"). expect("error getting jar_file value from wrapper_config");
-    let gui = config_data.get("gui").           expect("error getting gui value from wrapper_config");
-    let xmx = config_data.get("xmx").           expect("error getting xmx value from wrapper_config");
-    let xms = config_data.get("xms").           expect("error getting xms value from wrapper_config");
+    let jar_file = config_data.get("jar_file").         expect("error: jar_file value does not exist in wrapper_config");
+    let gui = config_data.get("gui").                   expect("error: gui value does not exist in wrapper_config");
+    let xmx = config_data.get("xmx").                   expect("error: xmx value does not exist in wrapper_config");
+    let xms = config_data.get("xms").                   expect("error: xms value does not exist in wrapper_config");
+    config_data.get("api-key").                                  expect("error: api-key value does not exist in wrapper_config");
+    let _port: u16 = config_data.get("api-port").                expect("error: api-port value does not exist in wrapper_config").parse().expect("error: api-port is not a valid number");
 
     println!(r#"Using JAR file: "{jar_file}""#);
 
-    thread::spawn(|| {
+    tokio::spawn(async {
         loop {
             let mut buffer = String::new();
             io::stdin().read_line(&mut buffer).expect("failed to read stdin");
@@ -53,8 +62,12 @@ fn main() {
         }
     });
 
-    if Path::new(jar_file).exists() {
-        wrapper::init(jar_file, xmx, xms, gui);
+    if Path::new(&jar_file).exists() {
+        wrapper::init(jar_file, xmx, xms, gui).await;
+
+        tokio::task::spawn_blocking(|| {
+            network::main();
+        }).await.expect("API panicked")
     } else {
         println!(r#"JAR file: "{jar_file}" was not found!"#);
         std::process::exit(1);
--- src/wrapper.rs
+++ src/wrapper.rs
@@ -22,7 +22,7 @@ pub struct Player<'a> {
     pub uuid: &'a str,
 }
 
-pub fn init(path: &str, xmx: &str, xms: &str, gui: &str) {
+pub async fn init(path: &str, xmx: &str, xms: &str, gui: &str) {
     println!("Initializing wrapper");
     
     unsafe {
@@ -32,70 +32,72 @@ pub fn init(path: &str, xmx: &str, xms: &str, gui: &str) {
             ..Default::default()
         }).expect("failed to execute"));
 
-        let mut uuids: HashMap<String,String> = HashMap::new();
+        tokio::spawn(async move {
+            let mut uuids: HashMap<String,String> = HashMap::new();
 
-        match &mut P {
-            Some(c) => {
-                if let Some(stdout) = &mut c.stdout {
-                    let reader = BufReader::new(stdout);
-                    let lines = reader.lines();
-                    for line in lines {
-                        let line = line.unwrap();
-            
-                        let line_type = get_type(&line);
-            
+            match &mut P {
+                Some(c) => {
+                    if let Some(stdout) = &mut c.stdout {
+                        let reader = BufReader::new(stdout);
+                        let lines = reader.lines();
+                        for line in lines {
+                            let line = line.unwrap();
+                
+                            let line_type = get_type(&line);
+                
 
-                        // TODO: Use anything but this
+                            // TODO: Use anything but this
 
-                        if line_type == MessageType::AddUuid {
-                            let playername = line.split("]: ").nth(1).unwrap().split("UUID of player ").nth(1).unwrap().split(" is").nth(0).unwrap().to_string();
-                            let uuid = line.split(" is ").nth(1).unwrap().to_string();
-                            uuids.insert(playername, uuid);
+                            if line_type == MessageType::AddUuid {
+                                let playername = line.split("]: ").nth(1).unwrap().split("UUID of player ").nth(1).unwrap().split(" is").nth(0).unwrap().to_string();
+                                let uuid = line.split(" is ").nth(1).unwrap().to_string();
+                                uuids.insert(playername, uuid);
 
-                        } else if line_type == MessageType::Join {
-                            let name: &str = line.split("]: ").nth(1).unwrap().split("[").nth(0).unwrap().trim();
-                            let uuid: &str = uuids.get(name).unwrap();
+                            } else if line_type == MessageType::Join {
+                                let name: &str = line.split("]: ").nth(1).unwrap().split("[").nth(0).unwrap().trim();
+                                let uuid: &str = uuids.get(name).unwrap();
 
-                            join_event::fire_event(Player {
-                                name,
-                                uuid
-                            });
-                            
-                        } else if line_type == MessageType::Leave {
-                            let name: &str = line.split("]: ").nth(1).unwrap().split(" lost connection: ").nth(0).unwrap();
-                            let reason: &str = line.split("lost connection: ").nth(1).unwrap();
-                            let uuid = uuids.get(name);
-                            if uuid == None {
-                                continue;
+                                join_event::fire_event(Player {
+                                    name,
+                                    uuid
+                                });
+                                
+                            } else if line_type == MessageType::Leave {
+                                let name: &str = line.split("]: ").nth(1).unwrap().split(" lost connection: ").nth(0).unwrap();
+                                let reason: &str = line.split("lost connection: ").nth(1).unwrap();
+                                let uuid = uuids.get(name);
+                                if uuid == None {
+                                    continue;
+                                }
+
+                                leave_event::fire_event(Player {
+                                    name,
+                                    uuid: uuid.unwrap()
+                                }, reason);
+
+                            } else if line_type == MessageType::Chat {
+                                let name: &str = line.split("<").nth(1).unwrap().split(">").nth(0).unwrap();
+                                let content: &str = line.split("> ").nth(1).unwrap();
+
+                                chat_event::fire_event(Player {
+                                    name, uuid: uuids.get(name).unwrap() 
+                                }, content);
+                                
+                            } else if line_type == MessageType::ServerStart {
+                                start_event::fire_event();
+
+                            } else if line_type == MessageType::ServerClose {
+                                stop_event::fire_event();
+
                             }
-
-                            leave_event::fire_event(Player {
-                                name,
-                                uuid: uuid.unwrap()
-                            }, reason);
-
-                        } else if line_type == MessageType::Chat {
-                            let name: &str = line.split("<").nth(1).unwrap().split(">").nth(0).unwrap();
-                            let content: &str = line.split("> ").nth(1).unwrap();
-
-                            chat_event::fire_event(Player {
-                                name, uuid: uuids.get(name).unwrap() 
-                            }, content);
-                            
-                        } else if line_type == MessageType::ServerStart {
-                            start_event::fire_event();
-
-                        } else if line_type == MessageType::ServerClose {
-                            stop_event::fire_event();
-
+                
+                            println!("{}", line);
                         }
-            
-                        println!("{}", line);
                     }
-                }
-            },
-            None => println!("Child is null!"),
-        }
+                },
+                None => println!("Child is null!"),
+            }
+        });
     }
 }
 
