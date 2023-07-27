841d2f94b56115c9ca61ae590a5404cc52073d80
MrSlimeDiamond <findlayrichardson3@gmail.com>
2023-07-28 10:02:45 +1200

    Wildcard user authenticator
    I don't actually know if this works because I haven't been able to build the project on my laptop, but I'm pretty sure it will work.

--- src/regex_checker.rs
+++ src/regex_checker.rs
@@ -18,7 +18,7 @@ pub fn get_type(string: &str) -> MessageType {
         (Regex::new(r"\[.*\] \[Server thread\/INFO\]: <.*> .*").unwrap(),                   MessageType::Chat),
         (Regex::new(r"\[.*\] \[Server thread\/INFO\]: Done").unwrap(),                      MessageType::ServerStart),
         (Regex::new(r"\[.*\] \[Server thread\/INFO\]: Stopping the server").unwrap(),       MessageType::ServerClose),
-        (Regex::new(r"\[.*\] \[User Authenticator #1/INFO\]: UUID of player").unwrap(),     MessageType::AddUuid)
+        (Regex::new(r"\[.*\] \[User Authenticator .*/INFO\]: UUID of player").unwrap(),     MessageType::AddUuid)
     ];
 
     for (regex, message_type) in types {
