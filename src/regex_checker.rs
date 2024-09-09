534551134424ff91141a352a18329e835db4c92a
Findlay Richardson <findlayrichardson3@gmail.com>
2024-09-10 07:50:14 +1200

    Better regex matches
    now works with Paper servers from my light testing

--- src/regex_checker.rs
+++ src/regex_checker.rs
@@ -13,12 +13,12 @@ pub enum MessageType {
 
 pub fn get_type(string: &str) -> MessageType {
     let types: Vec<(Regex, MessageType)> = vec![
-        (Regex::new(r"\[.*\] \[Server thread\/INFO\]: .*\[\/.*\]").unwrap(),                MessageType::Join),
-        (Regex::new(r"\[.*\] \[Server thread\/INFO\]: .* lost connection: .*").unwrap(),    MessageType::Leave),
-        (Regex::new(r"\[.*\] \[Server thread\/INFO\]: <.*> .*").unwrap(),                   MessageType::Chat),
-        (Regex::new(r"\[.*\] \[Server thread\/INFO\]: Done").unwrap(),                      MessageType::ServerStart),
-        (Regex::new(r"\[.*\] \[Server thread\/INFO\]: Stopping the server").unwrap(),       MessageType::ServerClose),
-        (Regex::new(r"\[.*\] \[User Authenticator .*/INFO\]: UUID of player").unwrap(),     MessageType::AddUuid)
+        (Regex::new(r"INFO\]: .*\[\/.*\] logged in.*").unwrap(),    MessageType::Join),
+        (Regex::new(r"INFO\]: .* lost connection: .*").unwrap(),    MessageType::Leave),
+        (Regex::new(r"INFO\]: <.*> .*").unwrap(),                   MessageType::Chat),
+        (Regex::new(r"INFO\]: Done").unwrap(),                      MessageType::ServerStart),
+        (Regex::new(r"INFO\]: Stopping the server").unwrap(),       MessageType::ServerClose),
+        (Regex::new(r"INFO\]: UUID of player").unwrap(),            MessageType::AddUuid)
     ];
 
     for (regex, message_type) in types {
